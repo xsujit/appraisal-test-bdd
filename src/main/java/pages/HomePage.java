4beeaca580550de489c9f936b7a5e3ae775f3e80
Sujit <xsujit@gmail.com>
2020-01-04 20:28:05 +0000

    Added web elements

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,7 +8,21 @@ public class HomePage {
     @FindBy(css = ".display-3")
     WebElement banner;
 
+    @FindBy(linkText = "Register")
+    WebElement registerLink;
+
+    @FindBy(linkText = "Login")
+    WebElement loginLink;
+
     public String getBanner() {
         return banner.getText();
+    }
+
+    public void clickRegisterLink() {
+        registerLink.click();
+    }
+
+    public void clickLoginLink() {
+        loginLink.click();
     }
 }
