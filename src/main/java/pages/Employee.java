2763dcd9dfcd97aee834902c407f5af66ffeaedd
Sujit <xsujit@gmail.com>
2020-01-13 20:17:46 +0000

    Added Employee object

---
+++ src/main/java/pages/Employee.java
@@ -0,0 +1,30 @@
+package pages;
+
+import java.util.Map;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+
+    public Employee(Map<String, String> userDetails) {
+        userDetails.forEach((k, v) -> {
+            switch (k) {
+                case "firstName":
+                    this.firstName = v;
+                case "lastName":
+                    this.lastName = v;
+                case "username":
+                    this.username = v;
+                case "email":
+                    this.email = v;
+            }
+        });
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
