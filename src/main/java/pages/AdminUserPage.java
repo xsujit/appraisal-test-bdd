1dc115143e45f1da1b6311c65e97874f8b390b9e
Sujit <xsujit@gmail.com>
2020-01-25 22:50:17 +0000

    Added Page

---
+++ src/main/java/pages/AdminUserPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminUserPage {
+
+    @FindBy(tagName = "form")
+    protected WebElement userForm;
+
+    public List<String> getPendingUsers() {
+        List<String> users = new ArrayList<>();
+        WebElement table = userForm.findElements(By.tagName("table")).get(0);
+        List<WebElement> userRows = table.findElements(By.tagName("tr"));
+        for (WebElement userRow : userRows) {
+            List<WebElement> userRecord = userRow.findElements(By.tagName("td"));
+            users.add(userRecord.get(4).getText());
+        }
+        return users;
+    }
+}
