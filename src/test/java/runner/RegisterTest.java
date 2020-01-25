143266e0c6bb3802dd19f8311d86a8fdadaadf6f
Sujit <xsujit@gmail.com>
2020-01-25 22:50:46 +0000

    Experiment with ITestContext

--- src/test/java/runner/RegisterTest.java
+++ src/test/java/runner/RegisterTest.java
@@ -2,6 +2,9 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 
 @CucumberOptions(
         features = "classpath:features",
@@ -9,4 +12,16 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@RegistrationTest"
 )
 public class RegisterTest extends AbstractTestNGCucumberTests {
+
+    @BeforeMethod
+    public void setup(ITestContext scenario) {
+        System.out.println("Starting scenario " + scenario.getName());
+    }
+
+    @AfterTest
+    public void tearDown(ITestContext scenario) {
+        System.out.println("Status " + scenario.getPassedTests().toString());
+    }
+
 }
+
