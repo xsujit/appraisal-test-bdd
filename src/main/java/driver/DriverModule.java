f214e8cddf543cb8b8497ad2030ca770b3ab6a49
Sujit <xsujit@gmail.com>
2020-02-02 12:08:16 +0000

    Using bind instead of provides

--- src/main/java/driver/DriverModule.java
+++ src/main/java/driver/DriverModule.java
@@ -1,32 +1,31 @@
 package driver;
 
-import annotations.ChromeBrowser;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
+import com.google.inject.Scopes;
+import com.google.inject.name.Names;
 
 public class DriverModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        /*
+
         bind(DriverManager.class)
+                .annotatedWith(Names.named("CHROME"))
                 .to(ChromeDriverManager.class)
                 .in(Scopes.SINGLETON);
-         */
     }
 
     /*@Provides
     public WebDriver getDriver(@ChromeBrowser DriverManager driverManager) {
         System.out.println("getDriver");
         return driverManager.getDriver();
-    }*/
+    }
 
     @Provides
     @ChromeBrowser
     @Singleton
     public DriverManager getChromeDriverManager() {
         return new ChromeDriverManager();
-    }
+    }*/
 
 }
