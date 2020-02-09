a48764b93548eaaf2f9efcdfafca51cb9daff200
Sujit <xsujit@gmail.com>
2020-02-09 11:18:54 +0000

    Simplified test

--- src/test/java/runner/LoginPageTest.java
+++ src/test/java/runner/LoginPageTest.java
@@ -1,65 +1,18 @@
 package runner;
 
-import com.google.inject.Inject;
-import gherkin.events.PickleEvent;
-import gherkin.pickles.PickleStep;
-import guice.AppModule;
-import io.cucumber.testng.CucumberFeatureWrapper;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.PickleEventWrapper;
-import io.cucumber.testng.TestNGCucumberRunner;
-import org.testng.annotations.*;
-import pages.PageUtil;
-
-import java.util.List;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features",
         glue = "step_definitions.login",
         tags = "@LoginTest"
 )
-@Guice(modules = {AppModule.class})
-public class LoginPageTest {
-
-    private PageUtil pageUtil;
-    private TestNGCucumberRunner testNGCucumberRunner;
-
-    @Inject
-    public LoginPageTest(PageUtil pageUtil) {
-        this.pageUtil = pageUtil;
-    }
-
-    @BeforeClass(alwaysRun = true)
-    public void setUpClass() {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    }
-
-    @Test(
-            groups = "Cucumber",
-            description = "Runs Cucumber Feature",
-            dataProvider = "scenarios"
-    )
-    public void scenario(PickleEventWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) throws Throwable {
-        System.out.println("Feature: " + cucumberFeature.toString());
-        PickleEvent event = pickleEvent.getPickleEvent();
-        System.out.println("Scenario:  " + event.pickle.getName());
-        List<PickleStep> steps = event.pickle.getSteps();
-        for (PickleStep step : steps) {
-            System.out.println(step.getText());
-        }
-        testNGCucumberRunner.runScenario(event);
-    }
+public class LoginPageTest extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
-        return this.testNGCucumberRunner == null ? new Object[0][0] : this.testNGCucumberRunner.provideScenarios();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() {
-        if (this.testNGCucumberRunner != null) {
-            pageUtil.getDriverManager().quitDriver();
-            this.testNGCucumberRunner.finish();
-        }
+        return super.scenarios();
     }
 }
--- src/test/java/runner/RegisterTest.java
+++ src/test/java/runner/RegisterTest.java
@@ -1,15 +1,7 @@
 package runner;
 
-import io.cucumber.core.api.Scenario;
-import io.cucumber.core.event.Status;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.TestNGCucumberRunner;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-
-import java.util.Arrays;
 
 @CucumberOptions(
         features = "classpath:features",
@@ -18,32 +10,5 @@ import java.util.Arrays;
 )
 public class RegisterTest extends AbstractTestNGCucumberTests {
 
-    private TestNGCucumberRunner testNGCucumberRunner;
-    // private ITestContext iTestContext;
-
-    public RegisterTest() {
-        this.testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    }
-
-    @BeforeMethod
-    public void setup() {
-
-        System.out.println("Starting scenario " + Arrays.deepToString(provideScenarios()));
-    }
-
-    @AfterTest
-    public void tearDown() {
-        System.out.println("Status ");
-    }
-
-    @DataProvider
-    public Object[][] provideScenarios() {
-        return testNGCucumberRunner.provideScenarios();
-    }
-
-    @DataProvider
-    public Object[] getName(Scenario scenario) {
-        return new Status[]{scenario.getStatus()};
-    }
 }
 
