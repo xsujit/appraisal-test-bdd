cf403880601517df158fffcef0a2f387a3e93ce2
Sujit <xsujit@gmail.com>
2020-02-23 19:58:55 +0000

    Added parallel execution

--- src/test/java/com/appraisal/runner/RegressionTest.java
+++ src/test/java/com/appraisal/runner/RegressionTest.java
@@ -2,6 +2,7 @@ package com.appraisal.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features",
@@ -9,4 +10,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@LoginTest or @RegistrationTest"
 )
 public class RegressionTest extends AbstractTestNGCucumberTests {
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
